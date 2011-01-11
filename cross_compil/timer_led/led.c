927b1cea1db23a3d38d3f8ae2fe47adfb3d4f306
Samuel Mokrani <samuel.mokrani@gmail.fr>
2011-01-11 13:36:51 +0100

    bip and chenillard ok

--- cross_compil/timer_led/led.c
+++ cross_compil/timer_led/led.c
@@ -5,49 +5,36 @@
 
 void chenillard()
 {
-	unsigned short currentLed=4;
+  unsigned short currentLed=4;
 
-	switchAllOnState(OFF);
+  switchAllOnState(OFF);
 
-	while(1)
-	{
-		switchOnOff(currentLed,OFF);
-		currentLed=(currentLed%4)+1;
-		switchOnOff(currentLed,ON);
-		//wait(5);//5*100ms
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-		wait100ms();
-	}
+  while(1)
+  {
+	 switchOnOff(currentLed,OFF);
+	 currentLed=(currentLed%4)+1;
+	 switchOnOff(currentLed,ON);
+	 wait(1000);//1000*1ms
+  }
 }
 
 void switchAllOnState(short state)
 {
-	modif(&PORTC,MASQUE_C,(state==ON)?0xE:0x0);
-	switchOnOff(4,state);
+  modif(&PORTC,MASQUE_C,(state==ON)?0xE:0x0);
+  switchOnOff(4,state);
 }
 
 void switchOnOff(unsigned short ledNb, short state) 
 {
-	if(ledNb!=4)
-		modif(&PORTC,MASQUE_C ,(state==ON)?((unsigned short)1)<<ledNb:0);
-	else
-		modif(&PORTE,MASQUE_E,(state==ON)?0x20:0);
+  if(ledNb!=4)
+	 modif(&PORTC,MASQUE_C ,(state==ON)?((unsigned short)1)<<ledNb:0);
+  else
+	 modif(&PORTE,MASQUE_E,(state==ON)?0x20:0);
 }
 
 void modif(volatile unsigned long *registre, unsigned long masque, long val)
 {
-	*registre=(*registre&masque)|val;
+  *registre=(*registre&masque)|val;
 }
 
 //get state ? retoune val courante
