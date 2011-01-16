75714915fb7c3dbfd71f97d3b4ed613bfa24288d
Bertrand Chazot <chazot@enst.fr>
2011-01-16 22:43:06 +0100

    led : new version

--- cross_compil/timer_led/led.c
+++ cross_compil/timer_led/led.c
@@ -2,39 +2,38 @@
 #include "led.h"
 #include "timer.h"
 
-
 void chenillard()
 {
-  unsigned short currentLed=4;
+	unsigned short currentLed=4;
+	switchAllOnState(OFF);
 
-  switchAllOnState(OFF);
-
-  while(1)
-  {
-	 switchOnOff(currentLed,OFF);
-	 currentLed=(currentLed%4)+1;
-	 switchOnOff(currentLed,ON);
-	 wait(1000);//1000*1ms
-  }
+	while(1)
+	{
+		switchOnOff(currentLed,OFF);
+		currentLed=(currentLed%4)+1;
+		switchOnOff(currentLed,ON);
+		wait(1000);//1000*1ms
+	}
 }
 
 void switchAllOnState(short state)
 {
-  modif(&PORTC,MASQUE_C,(state==ON)?0xE:0x0);
-  switchOnOff(4,state);
+	modif(&PORTC,MASQUE_C,state);
+	modif(&PORTE,MASQUE_E,state);
 }
 
 void switchOnOff(unsigned short ledNb, short state) 
 {
-  if(ledNb!=4)
-	 modif(&PORTC,MASQUE_C ,(state==ON)?((unsigned short)1)<<ledNb:0);
-  else
-	 modif(&PORTE,MASQUE_E,(state==ON)?0x20:0);
+	if(ledNb!=4)
+		modif(&PORTC,((unsigned short)1)<<ledNb,state);
+	else
+		modif(&PORTE,MASQUE_E,state);
 }
 
-void modif(volatile unsigned long *registre, unsigned long masque, long val)
+void modif(volatile unsigned long *registre, unsigned long masque, short state)
 {
-  *registre=(*registre&masque)|val;
+	if(state==ON)
+		*registre=(*registre)|masque;
+	else
+		*registre=(*registre)&(~masque);
 }
-
-//get state ? retoune val courante
